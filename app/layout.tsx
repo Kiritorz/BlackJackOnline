8f43aa0ed68961fa401d7f728db21604dbf39089
k1ssinnlee <948655078@qq.com>
2024-06-02 16:51:50 +0800

    style:..

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
-            <main className="container mx-auto max-w-7xl flex-grow">
+            <main className="flex-grow">
               {children}
             </main>
           </div>
